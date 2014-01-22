143a1e682d95b5684d03ef25b9b47d242805e111
sportyfred <ivanboring@gmail.com>
2014-01-22 15:57:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,20 +9,13 @@ app.get('/', function(request, response){
 var activeClients = 0;
  
 //nodejs server listens to msgs on port 8080
-var server = app.listen(8080);
+var server = app.listen(3000);
 //io sockets would address to all the web-clients talking to this nodejs server
 var io = socket.listen(server);
  
 //using node-osc library: 'npm install node-osc'
 //this will also install 'osc-min'
-var osc = require('node-osc');
- 
- 
-//listening to OSC msgs to pass on to the web via nodejs
-var oscServer = new osc.Server(3334, '0.0.0.0');
-//sending OSC msgs to a client
-var oscClient = new osc.Client('130.229.146.234', 3333);
- 
+
 var message = new Array();
  
 var message2 = new Array();
@@ -49,43 +42,3 @@ function clientDisconnect(){
 }
      
  
-     
-    //received an osc msg
-    oscServer.on("message", function (msg, rinfo) {
-         
- 
-        console.log(rinfo);
-        message = msg[2];
-        message2 = msg[2];
-        console.log(message);
-        console.log(message[0]);
- 
-        if (message[0] == "/gyrosc/gyro"){
- 
-        //pass the msg on to all of the web-clients
-        //msg[1] stands for the first argument received which in this case should be a string
-        io.sockets.emit("gyro",message);
-        //io.sockets.emit("serverMsg",message[0]);
-        //io.sockets.emit("serverMsg1",message[1]);
-        //io.sockets.emit("serverMsg2",message[2]);
-        //io.sockets.emit("serverMsg3",message[3]);
-        //io.sockets.emit("serverMsg3",message[4]);
-    }
- 
- 
- 
-if (message[0] == "/gyrosc/accel"){
- 
-console.log(message);
- 
-        //pass the msg on to all of the web-clients
-        //msg[1] stands for the first argument received which in this case should be a string
-        io.sockets.emit("accel",message);
-        //io.sockets.emit("serverMsg",message[0]);
-        //io.sockets.emit("serverMsg1",message[1]);
-        //io.sockets.emit("serverMsg2",message[2]);
-        //io.sockets.emit("serverMsg3",message[3]);
-     
- 
-}
-});
